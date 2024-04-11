d84c811fc2e580f6c52d53084223504249c5001a
gilreich1 <gilreich12@gmail.com>
2024-04-11 12:05:15 +0100

    linting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,44 +1,52 @@
-import {AppPage} from "../types/AppTypes";
+import { AppPage } from '../types/AppTypes';
 const classNames = require('classnames');
 
 interface NavBarProps {
   currentPage: AppPage;
-  setCurrentPage: React.Dispatch<React.SetStateAction<AppPage>>
+  setCurrentPage: React.Dispatch<React.SetStateAction<AppPage>>;
 }
 
-const NavBar = ({currentPage, setCurrentPage}: NavBarProps) => {
-
+const NavBar = ({ currentPage, setCurrentPage }: NavBarProps) => {
   const onClick = (page: AppPage) => {
     setCurrentPage(page);
-  }
+  };
 
   return (
-    <div className={ 'NavBar' }>
+    <div className={'NavBar'}>
       <button
-        onClick={ () => onClick('users') }
+        onClick={() => onClick('users')}
         // className={ currentPage === 'users' ? 'NavBar__selected' : '' }>
         className={classNames('NavBar__tab', {
-          'NavBar__selected': currentPage === 'users'
-        })}>
+          NavBar__selected: currentPage === 'users',
+        })}
+      >
         Users
       </button>
       <button
-        onClick={ () => onClick("compostStands") }
+        onClick={() => onClick('compostStands')}
         className={classNames('NavBar__tab', {
-          'NavBar__selected': currentPage === 'compostStands'
+          NavBar__selected: currentPage === 'compostStands',
         })}
       >
         Compost Stands
       </button>
       <button
-        onClick={ () => onClick("transactions") }
+        onClick={() => onClick('transactions')}
         className={classNames('NavBar__tab', {
-          'NavBar__selected': currentPage === 'transactions'
+          NavBar__selected: currentPage === 'transactions',
         })}
       >
         Transactions
       </button>
+      <button
+        onClick={() => onClick('events')}
+        className={classNames('NavBar__tab', {
+          NavBar__selected: currentPage === 'events',
+        })}
+      >
+        Events
+      </button>
     </div>
-  )
-}
+  );
+};
 export default NavBar;
