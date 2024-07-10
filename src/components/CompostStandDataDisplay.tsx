6c62e415aa376a8700401d96c292eccf3d590ae6
gilreich1 <gilreich12@gmail.com>
2024-07-10 15:08:35 +0300

    fix

--- src/components/CompostStandDataDisplay.tsx
+++ src/components/CompostStandDataDisplay.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react';
+import { CompostStandDataDTO } from '../types/ApiTypes';
+import { fetchCompostStandData } from '../apiServices/CompostStandAPI';
+import { createCompostStandData } from '../utils/CompostStandUtils';
+import CompostStandDataItem from './CompostStandDataItem';
 import { useAppDispatch } from '../utils/hooks';
 import { setIsModalVisible, setLoading, setModalText } from '../store/appSlice';
 
@@ -5,14 +10,17 @@ const initialUserData: CompostStandDataDTO = {
   depositsWeightsByStands: [
     {
       id: '6',
-      name: "alexander_zaid",
-      weight: 0
-    }
+      name: 'alexander_zaid',
+      weight: 0,
+    },
   ],
-  period: 30
-}
+  period: 30,
+};
 
 const CompostStandDataDisplay = () => {
+  const [compostStandData, setUserData] =
+    useState<CompostStandDataDTO>(initialUserData);
+  const [period, setPeriod] = useState<number>(30);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -36,30 +44,26 @@ const CompostStandDataDisplay = () => {
   }, [period]);
 
   return (
-    <div className={ 'DataDisplay' }>
-      <h2 className={ 'DataDisplay__title' }>
-        Compost Stand Data
-      </h2>
-      <div>
-        PERIOD: { period } days
-      </div>
+    <div className={'DataDisplay'}>
+      <h2 className={'DataDisplay__title'}>Compost Stand Data</h2>
+      <div>PERIOD: {period} days</div>
       <input
-        defaultValue={ 30 }
-        type={ 'number' }
-        onChange={ e => setPeriod(parseInt(e.target.value)) }
-        min={ 0 }
+        defaultValue={30}
+        type={'number'}
+        onChange={(e) => setPeriod(parseInt(e.target.value))}
+        min={0}
       />
-      {
-        createCompostStandData(compostStandData.depositsWeightsByStands)
-          .map(compostStand => (
-          <CompostStandDataItem compostStand={compostStand} />
-        ))
-      }
-      <div>
-
-      </div>
+      {createCompostStandData(compostStandData.depositsWeightsByStands).map(
+        (compostStand) => (
+          <CompostStandDataItem
+            key={compostStand.id}
+            compostStand={compostStand}
+          />
+        )
+      )}
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
 export default CompostStandDataDisplay;
